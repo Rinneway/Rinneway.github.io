b01d8e0f373b38b7a411209cca739ce735771c1e
riNN <danilgreencat@gmail.com>
2026-04-15 18:26:16 +0500

    Update game.js

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ const cam = { x: 0, y: 0, shakeX: 0, shakeY: 0, shakeTimer: 0 };
 // State
 const G = {
   screen: "intro",   // intro | playing | levelEnd | gameover | lock | win
-  level:  2,
+  level:  0,
   codes:  ['-', '-', '-'],
   lives:  gameConfig.lives,
   tick:   0,

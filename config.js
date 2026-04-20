84d46da67b5cadc0fb8cefe84fac062118df5889
riNN <danilgreencat@gmail.com>
2026-04-20 20:50:42 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,21 @@
 const gameConfig = {
   levelCodes: ["0", "6", "7"],
+  start_level: 0,
 
   finalMessage: "Ты прошла все уровни.\nСобрала все сердца.\nТы у меня самая классная!\nПоэтому хочу кое-что сказать:\nЯ тебя люблю.",
 
   lives: 5,
 
   // Физика
-  gravity:        0.52,
-  jumpPower:     -9.0,
-  playerSpeed:    4.5,
+  gravity: 0.52,
+  jumpPower: -9.0,
+  playerSpeed: 4.5,
   groundFriction: 0.70,
-  airFriction:    0.86,
-  maxFallSpeed:   18,
+  airFriction: 0.86,
+  maxFallSpeed: 18,
 
   // Coyote time (можно прыгнуть N тиков после ухода с края)
-  coyoteFrames:    9,
+  coyoteFrames: 9,
   // Jump buffer (прыжок засчитывается если нажат за N тиков до приземления)
   jumpBufferFrames: 10,
 
@@ -22,6 +23,6 @@ const gameConfig = {
   soundEnabled: true,
 
   // Размер Canvas
-  canvasWidth:  800,
+  canvasWidth: 800,
   canvasHeight: 400,
 };
